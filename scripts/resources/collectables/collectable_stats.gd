12a7b72974080306116eb779657409014b0aaebd
prodcautious <125836095+prodcautious@users.noreply.github.com>
2024-07-21 12:40:47 -0400

    Reworking Collectables and Player Stats
    Removed some crappy code. Trying a new way of spawning resource nodes around the tilemap along with giving different stats to different villager types.

---
+++ scripts/resources/collectables/collectable_stats.gd
@@ -0,0 +1,9 @@
+class_name CollectableStats
+extends Resource
+
+@export var starting_health = 3
+@export var max_health = 3
+@export var resource_type: String
+@export var drop_type1: String
+@export var drop_type2: String
+@export var texture: Texture2D
